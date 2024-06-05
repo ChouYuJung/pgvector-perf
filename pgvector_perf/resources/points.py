611238ac8d65b649a6c7169fa7a161a00cf4c88d
Allen Chou <f1470891079@gmail.com>
2024-06-05 18:33:26 +0800

    Init point methods.

--- pgvector_perf/resources/points.py
+++ pgvector_perf/resources/points.py
@@ -1,4 +1,6 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Text
+
+from pgvector_perf.schemas import PointWithEmbeddingSchema
 
 if TYPE_CHECKING:
     from pgvector_perf.client import PgvectorPerf
@@ -10,3 +12,15 @@ class Points:
 
     def __init__(self, client: "PgvectorPerf"):
         self._client = client
+
+    def retrieve(self, id: Text, *args, **kwargs):
+        pass
+
+    def create(self, point: PointWithEmbeddingSchema, *args, **kwargs):
+        pass
+
+    def update(self, id: Text, point: PointWithEmbeddingSchema, *args, **kwargs):
+        pass
+
+    def delete(self, id: Text, *args, **kwargs):
+        pass
