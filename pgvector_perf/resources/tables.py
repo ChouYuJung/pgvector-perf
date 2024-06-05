aef490766cf4e4326ff1ba48054f1fa36c5bbd47
Allen Chou <f1470891079@gmail.com>
2024-06-05 15:36:49 +0800

    table create.

--- pgvector_perf/resources/tables.py
+++ pgvector_perf/resources/tables.py
@@ -1,6 +1,8 @@
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
+    from sqlalchemy.sql.schema import MetaData
+
     from pgvector_perf.client import PgvectorPerf
 
 
@@ -10,3 +12,12 @@ class Tables:
 
     def __init__(self, client: "PgvectorPerf"):
         self._client = client
+
+    def touch(self, *args, **kwargs):
+        self.create(*args, **kwargs)
+
+    def create(self, *args, **kwargs):
+        engine = self._client.engine
+        metadata: "MetaData" = self._client.model._sql_model.metadata
+
+        metadata.create_all(engine, checkfirst=True)
