dd3649679427f4fd73dcb4216318e56c9dd7efc2
Allen Chou <f1470891079@gmail.com>
2024-06-06 16:16:26 +0800

    Add created_at field to PointWithEmbedding model and schema
    - Import datetime and pytz
    - Add created_at field to PointWithEmbedding model
      - Use DateTime type
      - Add index
      - Set default to current UTC time
    - Add created_at field to PointWithEmbeddingSchema
      - Use datetime type
      - Set default factory to return current UTC time

--- pgvector_perf/schemas.py
+++ pgvector_perf/schemas.py
@@ -1,8 +1,10 @@
+from datetime import datetime
 from typing import List, Text, Type
 
+import pytz
 from pgvector.sqlalchemy import Vector
-from pydantic import BaseModel, ConfigDict, PrivateAttr
-from sqlalchemy import Integer, String
+from pydantic import BaseModel, ConfigDict, Field, PrivateAttr
+from sqlalchemy import DateTime, Integer, String
 from sqlalchemy.orm import declarative_base, mapped_column
 
 from pgvector_perf.config import settings
@@ -21,6 +23,12 @@ class PointWithEmbedding(Base):
     text = mapped_column(String, nullable=False)
     model = mapped_column(String, index=True, nullable=False, default="default")
     embedding = mapped_column(Vector(settings.vector_dimensions), nullable=False)
+    created_at = mapped_column(
+        DateTime,
+        index=True,
+        nullable=False,
+        default_factory=lambda: datetime.now(tz=pytz.utc),
+    )
 
 
 class PointWithEmbeddingSchema(BaseModel):
@@ -30,6 +38,7 @@ class PointWithEmbeddingSchema(BaseModel):
     text: Text
     model: Text
     embedding: List[float]
+    created_at: datetime = Field(default_factory=lambda: datetime.now(tz=pytz.utc))
 
     _sql_model: Type[PointWithEmbedding] = PrivateAttr(default=PointWithEmbedding)
 
